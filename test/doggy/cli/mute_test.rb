dc2ea3cd85ca76c45431d87b765e1871eb49d410
Elvin Efendi <elvin.efendiyev@gmail.com>
2016-12-14 14:27:08 -0500

    test for mute/unmute commands

--- test/doggy/cli/mute_test.rb
+++ test/doggy/cli/mute_test.rb
@@ -2,18 +2,19 @@ require_relative '../../test_helper'
 
 class Doggy::CLI::MuteTest < Minitest::Test
   def test_run
-    monitor = Doggy::Models::Monitor.new(load_fixture('monitor.json'))
-    Doggy::Models::Monitor.expects(:find).with(monitor.id).returns(monitor)
-    monitor.expects(:toggle_mute!).with('mute', '{}')
-    Doggy::CLI::Mute.new({}, [monitor.id]).run
-  end
- 
-  def test_run_mute_with_duration
+    mocked_run
     now = Time.new(2020, 1, 1, 1, 1, 1, '+00:00')
-    Time.expects(:now).returns(now)
+    Time.expects(:now).twice.returns(now)
+    mocked_run({ 'duration' => '4h' }, { end: 1577854861 })
+    mocked_run({ 'duration' => '4h', 'scope' => 'role:db' }, { end: 1577854861, scope: 'role:db' })
+  end
+
+  private
+
+  def mocked_run(options = {}, body = {})
     monitor = Doggy::Models::Monitor.new(load_fixture('monitor.json'))
     Doggy::Models::Monitor.expects(:find).with(monitor.id).returns(monitor)
-    monitor.expects(:toggle_mute!).with('mute', JSON.dump({ end: 1577854861 }))
-    Doggy::CLI::Mute.new({ 'duration' => '4h' }, [monitor.id]).run
+    monitor.expects(:toggle_mute!).with('mute', JSON.dump(body))
+    Doggy::CLI::Mute.new(options, [monitor.id]).run
   end
 end
